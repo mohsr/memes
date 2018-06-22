4e87a1cb552dcc34855e42c43da95b2423240b90
mohsr <mkrizvi98@gmail.com>
2018-06-22 16:06:29 -0600

    Revert "set heroku environment vars"
    This reverts commit f238bef56ca0be97e5f27c344d081c69108ea53b.

--- app.js
+++ app.js
@@ -10,24 +10,9 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'client')));
 
-/* GET home login page. */
+/* GET home page. */
 app.get('/', function(req, res) {
 	res.sendFile('index.html');
-});
-
-/* GET main meme page. */
-app.get(process.env.MEMEPAGE, function(req, res) {
-	res.sendFile(process.env.MEMEPAGE + '.html');
-});
-
-/* POST login password. */
-app.post('/login', function(req, res) {
-	var pwd = req.body.pwd;
-	if (pwd == null || pwd != process.env.MEMEPWD) {
-		res.sendStatus(404);
-	} else {
-		res.send('/main');
-	}
 });
 
 /* Listen on a port. */
