b21a66dcc1e400923877d4a6ef63359f2298dc22
mohsr <mkrizvi98@gmail.com>
2018-06-23 08:26:14 -0700

    updated todo some more

--- client/js/getMemes.js
+++ client/js/getMemes.js
@@ -12,8 +12,9 @@ $(document).ready(function() {
                 var memeshtml = "";
                 var data = JSON.parse(xhr.responseText);
                 for (var i = 0; i < data.length; i++) {
-                    memeshtml += ('<div class="meme">' + data[i].name + 
-                                  ' ' + data[i].txt + '</div><br/>');
+                    memeshtml += ('<div class="meme"><div class="memename">' + 
+                                  data[i].name + '</div><div class="memetxt"' +
+                                  ' ' + data[i].txt + '</div></div><br/>');
                 }
 
                 memes.innerHTML = memeshtml;
