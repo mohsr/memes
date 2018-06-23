42e6091ebd1f04b3596651417049d5f8a824e1b8
mohsr <mkrizvi98@gmail.com>
2018-06-23 11:03:17 -0700

    set request content header for POST /submitmeme

--- client/js/loginRequest.js
+++ client/js/loginRequest.js
@@ -1,18 +1,18 @@
 function loginRequest() {
-	var xhr = new XMLHttpRequest();
-	var pwd = ('pwd=' + $('#pwdinput').val());
-	xhr.open('POST', '/login');
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			if (xhr.status != 200) {
-				document.getElementById('errormsg').innerHTML = 
-					"Woops! Wrong password :(";
-			} else {
-				document.getElementById('errormsg').innerHTML = "";
-				window.location.href = ('/' + xhr.responseText);
-			}
-		}
-	}
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.send(pwd);
+    var xhr = new XMLHttpRequest();
+    var pwd = ('pwd=' + $('#pwdinput').val());
+    xhr.open('POST', '/login');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status != 200) {
+                document.getElementById('errormsg').innerHTML = 
+                    "Woops! Wrong password :(";
+            } else {
+                document.getElementById('errormsg').innerHTML = "";
+                window.location.href = ('/' + xhr.responseText);
+            }
+        }
+    }
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(pwd);
 }
--- client/js/submitMeme.js
+++ client/js/submitMeme.js
@@ -1,20 +1,21 @@
 function submitMeme() {
-	var name = $('#memename').val();
-	var txt  = $('#memetxt').val();
-	if (name == '' || txt == '') {
-		return;
-	}
-	var data = ('name=' + name + '&txt=' + txt);
+    var name = $('#memename').val();
+    var txt  = $('#memetxt').val();
+    if (name == '' || txt == '') {
+        return;
+    }
+    var data = ('name=' + name + '&txt=' + txt);
 
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', '/submitmeme');
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			if (xhr.status == 200) {
-				document.getElementById('ty').innerHTML = 
-					"Thanks for the meme!";
-			}
-		}
-	}
-	xhr.send(data);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/submitmeme');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                document.getElementById('ty').innerHTML = 
+                    "Thanks for the meme!";
+            }
+        }
+    }
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(data);
 }
