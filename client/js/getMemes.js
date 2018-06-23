266cd8d09df33288588430f06ceac5f9dac8c149
mohsr <mkrizvi98@gmail.com>
2018-06-23 00:36:31 -0600

    fixed typo when accessing xmlhttprequest members

--- client/js/getMemes.js
+++ client/js/getMemes.js
@@ -3,19 +3,17 @@ $(document).ready(function() {
     xhr.open('GET', '/getmemes');
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
-            if (xhr.readyState != 200) {
-                console.log("rip");
+            if (xhr.status != 200) {
                 document.getElementById('errormsg').innerHTML = 
                     "Sorry! Could not load the memes right now.";
             } else {
-                console.log("jk");
                 document.getElementById('errormsg').innerHTML = "";
                 var memes = document.getElementById('memes');
                 var memeshtml = "";
                 var data = JSON.parse(xhr.responseText);
                 for (var i = 0; i < data.length; i++) {
                     memeshtml += ('<div class="meme">' + data[i].name + 
-                                  ' ' + data[i].txt + '</div>');
+                                  ' ' + data[i].txt + '</div><br/>');
                 }
 
                 memes.innerHTML = memeshtml;
